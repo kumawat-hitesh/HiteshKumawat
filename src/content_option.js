94cfe75cb50fbfd1615d12ef3bff6c257cd8d4e9
Hitesh Kumawat <kumawathitesh2000@gmail.com>
2023-12-12 14:11:18 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,188 @@
+const logotext = "H I T E S H";
+const meta = {
+    title: "Hitesh Kumawat",
+    description: "I’m Hitesh Kumawat Full stack developer",
+};
+
+const introdata = {
+    title: "I’m Hitesh Kumawat",
+    animated: {
+        first: "I Love Coding",
+        second: "DSA Lover",
+        third: "I am a Programmer",
+        fourth: "I am a Full Stack Developer",
+    },
+    description: "To pursue a holistic career in Computer Science and IT field as a professional and excel the same. Always keep growing and learning new things.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "Hey there, I'm Hitesh, a software and application developer with a passion for creating seamless and delightful user experiences. I especially enjoy working on design operations and systems that help bring order to complexity.",
+};
+const worktimeline = [{
+    jobtitle: "Software developer at THE RYB Infotech",
+    where: "Udaipur",
+    date: "Mar 2023 - Present",
+},
+{
+    jobtitle: "Full stack web developer at Find N Vibe",
+    where: "Mumbai",
+    date: "Nov 2022 - Jan 2023",
+},
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [{
+    name: "Java",
+    value: 95,
+},
+{
+    name: "Dart",
+    value: 80,
+},
+{
+    name: "C++",
+    value: 85,
+},
+{
+    name: "C#",
+    value: 80,
+},
+{
+    name: "C",
+    value: 80,
+},
+{
+    name: "Python",
+    value: 70,
+},
+{
+    name: "Javascript",
+    value: 80
+},
+{
+    name: "HTML",
+    value: 90
+},
+{
+    name: "CSS",
+    value: 90
+},
+{
+    name: "SQL",
+    value: 95
+}
+
+
+];
+
+const services = [
+    {
+        title: "Mobile Apps",
+        description: "I develop beautiful and cross platform mobile apps using Flutter framework, .Net MAUI and Java/Kotlin.",
+    },
+    {
+        title: "Websites",
+        description: "I develop cool websites using HTML, CSS, Javascript and React js.",
+    },
+];
+
+const dataportfolio = [{
+    img: "https://media.istockphoto.com/id/1457066641/vector/social-media-network-concept-3d-icons-flying-over-smartphone-on-hand-drawn-sketch-doodle.jpg?s=1024x1024&w=is&k=20&c=ggCehn4eLHHNXL-1F26tc-toB9OKddJvsRFhAfV85n0=",
+    description: "Built a basic Social Media Application using Flutter and Firebase.",
+    link: "https://github.com/kumawat-hitesh/Social_Media_App",
+},
+{
+    img: "https://media.istockphoto.com/id/1428709516/photo/shopping-online-woman-hand-online-shopping-on-laptop-computer-with-virtual-graphic-icon.jpg?s=1024x1024&w=is&k=20&c=N5Fw7BZfKcYJMH9camA7rQ3q--7Ev7pKlQYEB_gPfo8=",
+    description: "Built an E-Commerce website for better shopping experience.",
+    link: "https://github.com/kumawat-hitesh/E-Commerce-Website",
+},
+{
+    img: "https://images.unsplash.com/photo-1611339555312-e607c8352fd7?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Built a Soptify Clone.",
+    link: "https://github.com/kumawat-hitesh/Spotify-Clone",
+},
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kumawathitesh2000@gmail.com",
+    YOUR_PHONE: "+91 9116605080",
+    description: "Hey there, I'm Hitesh, a software and application developer with a passion for creating seamless and delightful user experiences. I especially enjoy working on design operations and systems that help bring order to complexity.",
+    YOUR_SERVICE_ID: "service_p6ia08u",
+    YOUR_TEMPLATE_ID: "template_wpm02tk",
+    YOUR_USER_ID: "r7sJrDkevALtFSNf-",
+};
+
+const socialprofils = {
+    github: "https://github.com/kumawat-hitesh",
+    facebook: "https://www.facebook.com/itskumawatharsh",
+    linkedin: "https://www.linkedin.com/in/hitesh-kumawat-2855a323b/",
+    twitter: "https://twitter.com/itskumawatharsh",
+    instagram: "https://www.instagram.com/itskumawatharsh/",
+    leetcode: "https://leetcode.com/kumawathitesh/",
+    geeksforgeeks: "https://auth.geeksforgeeks.org/user/kumawathitesh",
+    codingninjas: "https://www.codingninjas.com/studio/profile/fbbd0578-e841-440d-be32-5bc286dbafbc",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
